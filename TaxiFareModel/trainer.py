080f02457bf9f23cb29f6c119778f09ac7f933a2
Leonard Theret <leonard.theret@edhec.com>
2020-11-24 15:11:57 +0100

    f

--- TaxiFareModel/trainer.py
+++ TaxiFareModel/trainer.py
@@ -51,6 +51,28 @@ class Trainer():
     def evaluate(self):
         return compute_rmse(self.pipeline.predict(self.X_test),self.y_test)
 
+    @memoized_property
+    def mlflow_client(self):
+        mlflow.set_tracking_uri(MLFLOW_URI)
+        return MlflowClient()
+
+    @memoized_property
+    def mlflow_experiment_id(self):
+        try:
+            return self.mlflow_client.create_experiment(self.experiment_name)
+        except BaseException:
+            return self.mlflow_client.get_experiment_by_name(self.experiment_name).experiment_id
+
+    @memoized_property
+    def mlflow_run(self):
+        return self.mlflow_client.create_run(self.mlflow_experiment_id)
+
+    def mlflow_log_param(self, key, value):
+        self.mlflow_client.log_param(self.mlflow_run.info.run_id, key, value)
+
+    def mlflow_log_metric(self, key, value):
+        self.mlflow_client.log_metric(self.mlflow_run.info.run_id, key, value)
+
 
 if __name__ == "__main__":
     df = get_data(nrows=10000)
